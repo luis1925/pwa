0d1612e84066c53ae4f9bb97fcb9884e5a4788fc
Luis1925 <luis.valderrama@2myr.com>
2019-01-15 14:26:56 -0500

    feat(project): vue pwa

---
+++ precache-manifest.eded6c781a7599e84194ee00842c5a19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa/robots.txt"
+  },
+  {
+    "revision": "38ab58e2b7bb27795d31",
+    "url": "/pwa/js/chunk-vendors.ee34a159.js"
+  },
+  {
+    "revision": "7b701d2272790a728a6b",
+    "url": "/pwa/js/app.f27700e7.js"
+  },
+  {
+    "revision": "82616ebbd75bde9faf4133ade29dd6e9",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "bc1efe42ae4f47c98dabfc40015a1fd5",
+    "url": "/pwa/img/logo.bc1efe42.png"
+  },
+  {
+    "revision": "eaa539030d84a8affba2810e368cacf1",
+    "url": "/pwa/img/logo-white.eaa53903.png"
+  },
+  {
+    "revision": "6d8386a3378267ce4677fceeb928154f",
+    "url": "/pwa/img/background.6d8386a3.png"
+  },
+  {
+    "revision": "7b701d2272790a728a6b",
+    "url": "/pwa/css/app.807eed2e.css"
+  }
+];
